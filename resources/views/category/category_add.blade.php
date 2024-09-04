cadb9e15d756f653c0ecd5020e1bb08a3df98332
georgemungai <george.mungai@aimsoft.co.ke>
2024-09-04 13:54:04 +0300

    Add-purchasing-to-items

--- resources/views/category/category_add.blade.php
+++ resources/views/category/category_add.blade.php
@@ -52,7 +52,7 @@
             },
             messages :{
                 name: {
-                    required : 'Please Enter Your Name',
+                    required : 'Please Enter A Category Name',
                 },
 
             },
@@ -74,4 +74,4 @@
 
 
 
-@endsection 
+@endsection
